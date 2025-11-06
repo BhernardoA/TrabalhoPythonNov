a1274e5030a18fdd7a7fe9b05ec137c405ca8b01
BhernardoA <164952746+BhernardoA@users.noreply.github.com>
2025-11-06 09:25:47 -0300

    Interface leitor de pdf

---
+++ a.py
@@ -0,0 +1,27 @@
+import requests as rq
+import PyPDF2
+import tkinter as tk
+from tkinter import filedialog
+
+def Get_text_from_PDFfiles_usingPyPDF2(in_PdfFile):
+    reader = PyPDF2.PdfReader(in_PdfFile) 
+    print(reader.pages[0].extract_text())
+
+def arquivo():
+    arquivo = filedialog.askopenfilename(title="Escolha um arquivo")
+    if arquivo:
+        print(arquivo)
+        
+
+def Interface():
+    root = tk.Tk()
+    root.title("Titulo da janela")
+    root.geometry("600x400")
+    Botao = tk.Button(root, text="escolha_o_arquivo", command=arquivo)
+    Botao.pack(pady=20)
+    tk.mainloop()
+
+
+if __name__ == '__main__':
+    Interface()
+    
