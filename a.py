8b876ace402238b64bc712a0b5456a42a26dd3f8
BhernardoA <164952746+BhernardoA@users.noreply.github.com>
2025-11-06 09:44:11 -0300

    Implementação da API

--- a.py
+++ a.py
@@ -7,6 +7,18 @@ def Get_text_from_PDFfiles_usingPyPDF2(in_PdfFile):
     reader = PyPDF2.PdfReader(in_PdfFile) 
     print(reader.pages[0].extract_text())
 
+def api():
+    url = "https://date.nager.at/api/v3/PublicHolidays/2025-2026/BR"
+
+    payload = {}
+    headers= {
+        'accept': 'applicantion/json',
+        'X-CSRF-TOKEN': 'pYBqfz7tfH5NFeqA2YXNhdZIsqRCMmef6FjOTNJ'
+        }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+
 def arquivo():
     arquivo = filedialog.askopenfilename(title="Escolha um arquivo")
     if arquivo:
